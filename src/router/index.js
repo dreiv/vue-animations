ed9d200d2a6c18f3262cb65b5bf901d70c912985
Andrei Voicu <dreivoicu@gmail.com>
2020-08-20 12:36:08 +0300

    starter

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Modal from '../views/Modal.vue'
+import List from '../views/List.vue'
 
 Vue.use(VueRouter)
 
@@ -11,13 +12,10 @@ Vue.use(VueRouter)
     component: Modal
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/list',
+    name: 'List',
+    component: List
+  },
 ]
 
 const router = new VueRouter({
